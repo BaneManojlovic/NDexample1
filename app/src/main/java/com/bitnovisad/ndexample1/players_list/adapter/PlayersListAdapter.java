929285708548c84902270ed09b231c42346ab16b
Branislav Manojlovic <bane1manojlovic@gmail.com>
2018-10-04 17:08:33 +0200

    Added PlayerDetailsFragment. Not Yet implemented.

--- app/src/main/java/com/bitnovisad/ndexample1/players_list/adapter/PlayersListAdapter.java
+++ app/src/main/java/com/bitnovisad/ndexample1/players_list/adapter/PlayersListAdapter.java
@@ -1,7 +1,12 @@
 package com.bitnovisad.ndexample1.players_list.adapter;
 
 import android.content.Context;
+import android.content.Intent;
 import android.support.annotation.NonNull;
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,11 +16,14 @@ import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bitnovisad.ndexample1.PlayerDetailsFragment;
 import com.bitnovisad.ndexample1.R;
 import com.bitnovisad.ndexample1.players_list.model.Player;
 import com.squareup.picasso.Picasso;
 
 import java.util.List;
+
+import jp.wasabeef.picasso.transformations.RoundedCornersTransformation;
 
 public class PlayersListAdapter extends RecyclerView.Adapter<PlayersListAdapter.ViewHolder> {
 
@@ -44,22 +52,34 @@ public class PlayersListAdapter extends RecyclerView.Adapter<PlayersListAdapter.
         //call Picasso to get image url
         Picasso.with(context)
                 .load(listItem.getPlayerImageUrl())
-                .resize(400,400)
+                .resize(400, 400)
                 .centerCrop()
+                .transform(new RoundedCornersTransformation(35, 10))
                 .into(holder.imageViewPlayer);
 
         //handling onclick on items in list
         holder.linearLayoutItem.setOnClickListener(new View.OnClickListener() {
+            Fragment selectedFragment = null;
+
             @Override
             public void onClick(View view) {
                 Toast.makeText(context, listItem.getPlayerName(), Toast.LENGTH_LONG).show();
 
                 //TODO - ubaci za otvaranje podataka na PlayerDetails ekranu
-                //Intent intent = new Intent(context, PlayerDetailsActivity.class);
-                //        intent.putExtra("image_url", listItem.getPlayerImageUrl());
-                //        intent.putExtra("ply_name", listItem.getPlayerName());
-                //        intent.putExtra("ply_position", listItem.getPlayerPosition());
-                //        context.startActivity(intent);
+                Intent intent = new Intent(context, PlayerDetailsFragment.class);
+                intent.putExtra("image_url", listItem.getPlayerImageUrl());
+                intent.putExtra("ply_name", listItem.getPlayerName());
+                intent.putExtra("ply_position", listItem.getPlayerPosition());
+                context.startActivity(intent);
+
+                //TODO- resi otvaranje fragmenta!!!
+//                        selectedFragment = new PlayerDetailsFragment();
+//                        context.getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container, selectedFragment).commit();
+                //FragmentManager fm = getFragmentManager();
+                //    FragmentTransaction ft = fm.beginTransaction();
+                //    FragmentGreen llf = new FragmentGreen();
+                //    ft.replace(R.id.listFragment, llf);
+                //    ft.commit();
 
             }
         });
@@ -71,7 +91,6 @@ public class PlayersListAdapter extends RecyclerView.Adapter<PlayersListAdapter.
     public int getItemCount() {
         return listItems.size();
     }
-
 
 
     public class ViewHolder extends RecyclerView.ViewHolder {
